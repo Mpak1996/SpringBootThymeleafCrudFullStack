fb891f38c27d22a2c37ddced5820d474d4e7ebab
Mpak1996 <giannismpak@outlook.com>
2021-06-17 13:57:45 +0300

    Create application properties and Employee.class for model Layer

---
+++ src/main/java/com/example/SpringBootFullStackThymeleaf/SpringBootFullStackThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootFullStackThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFullStackThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFullStackThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootFullStackThymeleaf/SpringBootFullStackThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootFullStackThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFullStackThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
