0b5377fd684002e80e99a55678d0489ac90b7532
Mpak1996 <giannismpak@outlook.com>
2021-07-14 12:54:17 +0300

    Update controller, service, serviceimpl and index.html - Insert new_employee.html

--- src/main/java/com/example/SpringBootFullStackThymeleaf/controller/EmployeeController.java
+++ src/main/java/com/example/SpringBootFullStackThymeleaf/controller/EmployeeController.java
@@ -1,10 +1,14 @@
 package com.example.SpringBootFullStackThymeleaf.controller;
 
+import com.example.SpringBootFullStackThymeleaf.model.Employee;
 import com.example.SpringBootFullStackThymeleaf.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 
@@ -18,5 +22,34 @@ public class EmployeeController {
     public String viewHomePage(Model model){
         model.addAttribute("listEmployees", employeeService.getAllEmployees());
         return "index";
+    }
+
+    @GetMapping("/showNewEmployeeForm")
+    public String showNewEmployeeForm(Model model){
+        // create model attribute to bind from data
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return  "new_employee";
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        //save employee to database
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
+
+    }
+
+    @GetMapping("/showFormUpdate/{id}")
+    public String showFormUpdate(@PathVariable ( value = "id") long id, Model model){
+
+            // get employees from the service
+
+        Employee employee = employeeService.getEmployeeById(id);
+
+            // set employee as a model attribute
+
+            model.addAttribute("employee", employee);
+            return  "update_employee";
     }
 }
--- src/main/java/com/example/SpringBootFullStackThymeleaf/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/SpringBootFullStackThymeleaf/service/EmployeeServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 
@@ -17,5 +18,24 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public List<Employee> getAllEmployees() {
             return  employeeRepository.findAll();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        this.employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee getEmployeeById(long id) {
+        Optional<Employee> optional = employeeRepository.findById(id);
+        Employee employee = null;
+        if (optional.isPresent()){
+            employee = optional.get();
+        }
+        else
+        {
+            throw new RuntimeException("Employee not found not found for id :: " + id);
+        }
+        return employee;
     }
 }
